4de4b8262dd2ffcfddcfbf2449b835bc54bfe049
sagarran1988 <sagarranga123@gmail.com>
2017-10-23 17:16:17 +0200

    Assignment Commit

--- assignment-directives/src/app/app.component.ts
+++ assignment-directives/src/app/app.component.ts
@@ -7,4 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  showSecret = false;
+  log = [];
+
+  onToggleDetails(){
+    this.showSecret = !this.showSecret;
+    this.log.push(this.log.length + 1);
+  }
 }
